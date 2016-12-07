6256a3f3c31b772bc76a06255f82c88f6f9703a1
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-12-07 15:32:52 +0100

    added test for array of props

--- test/index.js
+++ test/index.js
@@ -84,3 +84,44 @@ testRule(rule, {
     },
   ],
 })
+
+// array of properties
+testRule(rule, {
+  ruleName,
+  skipBasicChecks: true,
+
+  config: [['color', 'font-size', 'fill']],
+
+  accept: [
+    { code: '.foo { color: $bar; }' },
+    { code: '.foo { color: @bar; }' },
+    { code: '.foo { color: var(--bar); }' },
+    { code: '.foo { font-size: $bar; }' },
+    { code: '.foo { font-size: @bar; }' },
+    { code: '.foo { font-size: var(--bar); }' },
+    { code: '.foo { fill: $bar; }' },
+    { code: '.foo { fill: @bar; }' },
+    { code: '.foo { fill: var(--bar); }' },
+  ],
+
+  reject: [
+    {
+      code: '.foo { color: #fff; }',
+      message: `Expected variable or function for #fff of color (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { font-size: 16px; }',
+      message: `Expected variable or function for 16px of font-size (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { fill: #fff; }',
+      message: `Expected variable or function for #fff of fill (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+  ],
+})
