37771367d8cbf024437b1d28cbb6a1d82c42cb0f
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-12-07 18:10:23 +0100

    added ignoreVaribales test

--- test/index.js
+++ test/index.js
@@ -36,6 +36,55 @@ testRule(rule, {
   ],
 })
 
+// disabling ignoreVariables
+testRule(rule, {
+  ruleName,
+  skipBasicChecks: true,
+
+  config: ['color', {
+    ignoreVariables: false,
+  }],
+
+  accept: [
+    { code: '.foo { color: map-get($bar, baz); }' },
+    { code: '.foo { color: darken(#fff, 10%); }' },
+    { code: '.foo { color: color(#fff, lighten(10%)); }' },
+  ],
+
+  reject: [
+    {
+      code: '.foo { color: #fff; }',
+      message: `Expected function for "#fff" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: red; }',
+      message: `Expected function for "red" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: $bar; }',
+      message: `Expected function for "$bar" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: @bar; }',
+      message: `Expected function for "@bar" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: var(--bar); }',
+      message: `Expected function for "var(--bar)" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+  ],
+})
+
 // disabling ignoreFunction
 testRule(rule, {
   ruleName,
