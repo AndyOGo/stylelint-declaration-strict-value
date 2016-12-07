e4a2339134daec31c8ca9eedbb1493c354d0ab30
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-12-07 15:46:14 +0100

    added keywords test

--- test/index.js
+++ test/index.js
@@ -197,3 +197,26 @@ testRule(rule, {
     { code: '.foo { position: absolute; }' },
   ],
 })
+
+// ignore keywords
+testRule(rule, {
+  ruleName,
+  skipBasicChecks: true,
+
+  config: ['color', {
+    ignoreKeywords: 'transparent',
+  }],
+
+  accept: [
+    { code: '.foo { color: transparent; }' },
+  ],
+
+  reject: [
+    {
+      code: '.foo { color: inherit; }',
+      message: `Expected variable, function or keyword for inherit of color (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+  ],
+})
