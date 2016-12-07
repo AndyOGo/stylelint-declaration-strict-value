8f50233fd46e7a8b14565c5f39fbee081dcb2a75
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-12-07 15:47:56 +0100

    added test for multiple keywords

--- test/index.js
+++ test/index.js
@@ -220,3 +220,28 @@ testRule(rule, {
     },
   ],
 })
+
+// ignore multiple keywords
+testRule(rule, {
+  ruleName,
+  skipBasicChecks: true,
+
+  config: ['color', {
+    ignoreKeywords: ['transparent', 'currentColor', 'blue'],
+  }],
+
+  accept: [
+    { code: '.foo { color: transparent; }' },
+    { code: '.foo { color: currentColor; }' },
+    { code: '.foo { color: blue; }' },
+  ],
+
+  reject: [
+    {
+      code: '.foo { color: inherit; }',
+      message: `Expected variable, function or keyword for inherit of color (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+  ],
+})
