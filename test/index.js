a9cbd8ed7799d63fd0c5c2f9a24db07fb9e8798f
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-12-07 15:57:43 +0100

    added keywords hash validation

--- test/index.js
+++ test/index.js
@@ -245,3 +245,41 @@ testRule(rule, {
     },
   ],
 })
+
+// ignore keywords hash
+testRule(rule, {
+  ruleName,
+  skipBasicChecks: true,
+
+  config: [['/color$/', 'fill'], {
+    ignoreKeywords: {
+      '/color$/': ['transparent', 'currentColor'],
+      'fill': 'inherit',
+    },
+  }],
+
+  accept: [
+    { code: '.foo { color: transparent; }' },
+    { code: '.foo { color: currentColor; }' },
+    { code: '.foo { background-color: transparent; }' },
+    { code: '.foo { background-color: currentColor; }' },
+    { code: '.foo { border-color: transparent; }' },
+    { code: '.foo { border-color: currentColor; }' },
+    { code: '.foo { fill: inherit; }' },
+  ],
+
+  reject: [
+    {
+      code: '.foo { color: inherit; }',
+      message: `Expected variable, function or keyword for inherit of color (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { fill: currentColor; }',
+      message: `Expected variable, function or keyword for currentColor of fill (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+  ],
+})
