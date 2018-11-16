af04401614f7812979c02fc085a410fbf16ea906
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2018-11-16 01:31:43 +0100

    start adding var detection

---
+++ src/lib/get-node.js
@@ -0,0 +1,21 @@
+import {
+  reFunc, reSkipProp, reVar,
+} from './validation'
+
+const getNode = (node) => {
+  const rawValue = node.toString()
+
+  // IMPORTANT detect vars for CSS, SCSS and Less
+  if (reVar.test(rawValue)) {
+    /* eslint-disable no-param-reassign */
+    node.type = 'var'
+    // make sure to mark walk through children as skipped
+    node.skipChildren = true
+
+    return node
+  }
+
+  return node
+}
+
+export default getNode
