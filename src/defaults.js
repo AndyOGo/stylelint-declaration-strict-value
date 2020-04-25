0ced77b8c243d638715209016114ce59200012ed
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-04-25 11:59:44 +0200

    docs: mark ignoreKeywords as deprecated

--- src/defaults.js
+++ src/defaults.js
@@ -4,7 +4,7 @@
  * @typedef {object} SecondaryOptions
  * @property {boolean} [ignoreVariables=true] - Wheter or not to ignore variables.
  * @property {boolean} [ignoreFunctions=true] - Wheter or not to ignore function.
- * @property {null|number|string|Array|object} [ignoreKeywords=null] - A ignored keywords config.
+ * @property {null|number|string|Array|object} [ignoreKeywords=null] - **DEPRECATED:** A ignored keywords config.
  * @param {number|string|RegExp|Array|object} [actual.ignoreValues=null] - A ignored values config.
  * @property {string} [severity='error'] - Adjust severity of the rule, `'warning'` or `'error'` (default).
  * @property {null|string} [message=null] - A custom message when a rule is violated, interpolated with `${types}`, `${value}` and `${property}`.
@@ -18,7 +18,7 @@
  * @constant {SecondaryOptions}
  * @property {boolean} [ignoreVariables=true] - Ignore variables by default.
  * @property {boolean} [ignoreFunctions=true] - Ignore function by default.
- * @property {null} [ignoreKeywords=null] - Forbid keywords by default.
+ * @property {null} [ignoreKeywords=null] - **DEPRECATED:** Forbid keywords by default.
  * @property {null} [ignoreValues=null] - Forbid values by default.
  * @property {string} [severity='error'] - This rule's default severity is `'error'`.
  * @property {null} [message=null] - Use default message.
