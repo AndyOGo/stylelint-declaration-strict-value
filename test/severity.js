51feaa67ab1cb1f092a8c9efc780973df66c3375
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-04-26 10:31:17 +0200

    test: added severity option

---
+++ test/severity.js
@@ -0,0 +1,46 @@
+import testRule from 'stylelint-test-rule-tape'
+
+import declarationStrictValue, { ruleName } from '../src'
+
+const { rule } = declarationStrictValue
+
+// custom message
+testRule(rule, {
+  ruleName,
+  skipBasicChecks: true,
+
+  config: ['color', {
+    severity: 'warning',
+  }],
+
+  reject: [
+    {
+      code: '.foo { color: #fff; }',
+      message: `Expected variable or function for "#fff" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: red; }',
+      message: `Expected variable or function for "red" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+  ],
+})
+
+testRule(rule, {
+  ruleName,
+  skipBasicChecks: true,
+
+  config: ['color', {
+    severity: 1234,
+  }],
+
+  reject: [
+    {
+      code: '.foo { color: red; }',
+      message: `Invalid option "{"severity":1234}" for rule ${ruleName}`,
+    },
+  ],
+})
