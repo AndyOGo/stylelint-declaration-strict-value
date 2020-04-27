852ecf4b5b03b7ce687532bf5e148d68ecb598fe
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-04-28 00:33:51 +0200

    feat: added shorthand support

--- src/defaults.js
+++ src/defaults.js
@@ -6,6 +6,8 @@
  * @property {boolean} [ignoreFunctions=true] - Wheter or not to ignore function.
  * @property {null|number|string|Array|object} [ignoreKeywords=null] - **DEPRECATED:** An ignored keywords config.
  * @property {number|string|RegExp|Array|object} [ignoreValues=null] - An ignored values config.
+ * @property {boolean} [expandShorthand=true] - Wheter or not to expand shorthand CSS properties.
+ * @property {boolean} [recurseLonghand=false] - Wheter or not to expand longhand CSS properties recursivly - this is only useful for the border property.
  * @property {string} [severity='error'] - Adjust severity of the rule, `'warning'` or `'error'` (default).
  * @property {null|string} [message=null] - A custom message when a rule is violated, interpolated with `${types}`, `${value}` and `${property}`.
  * @property {boolean} [disableFix=false] - Don't auto-fix if `--fix` option is applied.
@@ -20,6 +22,8 @@
  * @property {boolean} [ignoreFunctions=true] - Ignore function by default.
  * @property {null} [ignoreKeywords=null] - **DEPRECATED:** Forbid keywords by default.
  * @property {null} [ignoreValues=null] - Forbid values by default.
+ * @property {boolean} [expandShorthand=true] - Expand shorthand CSS properties.
+ * @property {boolean} [recurseLonghand=false] - Do not expand longhand properties recursivly - only useful for `border`.
  * @property {string} [severity='error'] - This rule's default severity is `'error'`.
  * @property {null} [message=null] - Use default message.
  * @property {null} [disableFix=false] - Don't auto-fix if `--fix` option is applied.
@@ -31,6 +35,8 @@ const defaults = {
   ignoreFunctions: true,
   ignoreKeywords: null,
   ignoreValues: null,
+  expandShorthand: true,
+  recurseLonghand: false,
   severity: 'error',
   message: null,
   disableFix: false,
