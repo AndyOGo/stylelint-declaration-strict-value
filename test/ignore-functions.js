08db194c4c4279cdfb93dd52201eaa753de3e09e
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2020-09-06 10:31:16 +0200

    test: basic var and func hash

--- test/ignore-functions.js
+++ test/ignore-functions.js
@@ -135,6 +135,102 @@ testRule(rule, {
   ruleName,
   skipBasicChecks: true,
 
+  config: [['color', 'margin'], {
+    ignoreFunctions: {
+      margin: false,
+    },
+  }],
+
+  accept: [
+    { code: '.foo { color: $bar; }' },
+    { code: '.foo { color: namespace.$bar; }' },
+    { code: '.foo { color: @bar; }' },
+    { code: '.foo { color: var(--bar); }' },
+    { code: '.foo { color: var(--bar, fallback); }' },
+    { code: '.foo { color: var(--bar, fallback, fallback2); }' },
+    {
+      code: `.foo { color: var(
+        --bar,
+        fallback
+      ); }`,
+    },
+    {
+      code: `.foo { color: var(
+        --bar,
+        fallback,
+        fallback2
+      ); }`,
+    },
+    {
+      code: '.foo { color: map-get($bar, baz); }',
+    },
+    {
+      code: '.foo { color: map-get(namespace.$bar, baz); }',
+    },
+    {
+      code: '.foo { color: darken(#fff, 10%); }',
+    },
+    {
+      code: '.foo { color: color(#fff, lighten(10%)); }',
+    },
+    {
+      code: `.foo { color: map-get(
+        $bar,
+        baz)
+      ; }`,
+    },
+    {
+      code: `.foo { color: map-get(
+        namespace.$bar,
+        baz)
+      ; }`,
+    },
+    {
+      code: `.foo { color: darken(
+        #fff,
+        10%)
+      ; }`,
+    },
+    {
+      code: `.foo { color: color(
+        #fff,
+        lighten(10%))
+      ; }`,
+    },
+  ],
+
+  reject: [
+    {
+      code: '.foo { color: #fff; }',
+      message: `Expected variable or function for "#fff" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: red; }',
+      message: `Expected variable or function for "red" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: `.foo { margin: calc(
+        var(--x) *
+        var(--y)
+      ); }`,
+      message: `Expected variable or function for "calc(
+        var(--x) *
+        var(--y)
+      )" of "margin" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+  ],
+})
+
+testRule(rule, {
+  ruleName,
+  skipBasicChecks: true,
+
   config: ['color', {
     ignoreFunctions: 'foo',
   }],
--- test/ignore-variables.js
+++ test/ignore-variables.js
@@ -128,6 +128,130 @@ testRule(rule, {
   ruleName,
   skipBasicChecks: true,
 
+  config: [['color', 'margin'], {
+    ignoreVariables: {
+      color: false,
+    },
+  }],
+
+  accept: [
+    { code: '.foo { color: spacing(); }' },
+    { code: '.foo { color: map-get($bar, baz); }' },
+    { code: '.foo { color: map-get(namespace.$bar, baz); }' },
+    { code: '.foo { color: darken(#fff, 10%); }' },
+    { code: '.foo { color: color(#fff, lighten(10%)); }' },
+    {
+      code: `.foo { color: map-get(
+        $bar,
+        baz)
+      ; }`,
+    },
+    {
+      code: `.foo { color: map-get(
+        namespace.$bar,
+        baz)
+      ; }`,
+    },
+    {
+      code: `.foo { color: darken(
+        #fff,
+        10%)
+      ; }`,
+    },
+    {
+      code: `.foo { color: color(
+        #fff,
+        lighten(10%))
+      ; }`,
+    },
+    { code: '.foo { margin: namespace.$bar; }' },
+    { code: '.foo { margin: @bar; }' },
+    { code: '.foo { margin: var(--bar); }' },
+    {
+      code: `.foo { margin: calc(
+        var(--x) *
+        var(--y)
+      ); }`,
+    },
+  ],
+
+  reject: [
+    {
+      code: '.foo { color: #fff; }',
+      message: `Expected variable or function for "#fff" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: red; }',
+      message: `Expected variable or function for "red" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: $bar; }',
+      message: `Expected variable or function for "$bar" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: namespace.$bar; }',
+      message: `Expected variable or function for "namespace.$bar" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: @bar; }',
+      message: `Expected variable or function for "@bar" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: '.foo { color: var(--bar); }',
+      message: `Expected variable or function for "var(--bar)" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: `.foo { color: var(
+        --bar,
+        fallback
+      ); }`,
+      message: `Expected variable or function for "var(
+        --bar,
+        fallback
+      )" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: `.foo { color: var(
+        --bar,
+        fallback,
+        fallback2
+      ); }`,
+      message: `Expected variable or function for "var(
+        --bar,
+        fallback,
+        fallback2
+      )" of "color" (${ruleName})`,
+      line: 1,
+      column: 8,
+    },
+    {
+      code: `@value bar: #000;
+            .foo { color: bar; }`,
+      message: `Expected variable or function for "bar" of "color" (${ruleName})`,
+      line: 2,
+      column: 20,
+    },
+  ],
+})
+
+testRule(rule, {
+  ruleName,
+  skipBasicChecks: true,
+
   config: ['color', {
     ignoreVariables: 'foo',
   }],
