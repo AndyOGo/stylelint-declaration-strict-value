f073de4e0236a6cf10ea858c21291e8586924f09
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2021-02-25 09:07:53 +0100

    fix: eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  '(src|test)/**/*.[tj]s': (filenames) => [`eslint --fix ${filenames.join(' ')}`, 'node --require babel-register-ts test'],
+  '(src|test)/**/*.[tj]s': (filenames) => [
+    `eslint --fix ${filenames.join(' ')}`,
+    'node --require babel-register-ts test',
+  ],
   '(README).md': ["doctoc --title '**Table of Contents**'"],
-}
+};
